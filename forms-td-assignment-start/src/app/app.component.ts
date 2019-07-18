2feaa6ef07e1ca27a71ad7d39f89289874218540
Daniel Galván <danygs@gmail.com>
2019-07-18 16:05:08 -0500

    Angular course. Section 15, lesson 198

---
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  @ViewChild('f', { static: true }) signupForm: NgForm;
+  subscriptions = ['Basic', 'Advanced', 'Pro'];
+  defaultSubscription = 'Advanced';
+  user = {
+    email: '',
+    subscription: '',
+    password: ''
+  };
+  submitted = false;
+
+  onSubmit() {
+    this.submitted = true;
+    this.user.email = this.signupForm.value.email;
+    this.user.subscription = this.signupForm.value.subscriptions;
+    this.user.password = this.signupForm.value.password;
+
+    this.signupForm.reset();
+  }
+}
